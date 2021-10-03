d876afda460aeab3fb23fd393034d561049196f2
Michel Storms <stormychel@me.com>
2021-10-03 08:35:05 +0200

    Initial Commit

---
+++ TubesDemo/TubesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TubesDemoApp.swift
+//  TubesDemo
+//
+//  Created by Michel Storms on 03/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct TubesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
